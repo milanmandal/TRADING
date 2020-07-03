67463abf67e7eec472a23e924e0c41ba18af389f
milanmandal <51320381+milanmandal@users.noreply.github.com>
2020-07-03 18:54:33 +0530

    Updates

---
+++ precache-manifest.a4f356798cf92961747d8d1b11ff0221.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e8ae46455c74cac78dfc393790d787a",
+    "url": "/TRADING/index.html"
+  },
+  {
+    "revision": "4ce0d3325f1966946f9d",
+    "url": "/TRADING/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "22ecad2b12e8302c6ca2",
+    "url": "/TRADING/static/css/main.4e2462f7.chunk.css"
+  },
+  {
+    "revision": "4ce0d3325f1966946f9d",
+    "url": "/TRADING/static/js/2.80bbaafa.chunk.js"
+  },
+  {
+    "revision": "35ee28762e0a258d90de4629ab2cc087",
+    "url": "/TRADING/static/js/2.80bbaafa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22ecad2b12e8302c6ca2",
+    "url": "/TRADING/static/js/main.93f047a1.chunk.js"
+  },
+  {
+    "revision": "f47779587b985b782d5c",
+    "url": "/TRADING/static/js/runtime-main.58b1d47c.js"
+  }
+]);
